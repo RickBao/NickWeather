335ca94af6f1e8e9bb432e2275b0bb1a788c6832
Nick.mo <bjf@motern.cn>
2019-10-26 15:46:57 +0800

    添加entity下的具体Live Bean文件内容

--- app/src/main/java/com/example/myapplication/data/http/entity/envicloud/EnvironmentCloudCityAirLive.java
+++ app/src/main/java/com/example/myapplication/data/http/entity/envicloud/EnvironmentCloudCityAirLive.java
@@ -1,4 +1,138 @@
 package com.example.myapplication.data.http.entity.envicloud;
 
+import com.alibaba.fastjson.annotation.JSONField;
+
+/**
+ * 城市实时天气质量
+ */
 public class EnvironmentCloudCityAirLive {
+
+    @JSONField(name = "rcode")
+    private int requestCode;    //结果码
+
+    @JSONField(name = "rdesc")
+    private String requestDesc;     //结果描述
+
+    @JSONField(name = "citycode")
+    private String cityId;          //城市ID
+
+    private String time;            //时间
+
+    @JSONField(name = "AQI")
+    private String api;             //空气质量指数
+
+    @JSONField(name = "PM25")
+    private String pm25;            //PM2.5浓度
+
+    @JSONField(name = "PM10")
+    private String pm10;            //PM10浓度
+
+    @JSONField(name = "CO")
+    private String co;              //一氧化碳浓度
+
+    @JSONField(name = "SO2")
+    private String so2;             //二氧化硫浓度
+
+    @JSONField(name = "NO2")
+    private String no2;             //二氧化氮浓度
+
+    private String o3;              //臭氧浓度
+
+    private String primary;         //首要污染物
+
+    public int getRequestCode() {
+        return requestCode;
+    }
+
+    public void setRequestCode(int requestCode) {
+        this.requestCode = requestCode;
+    }
+
+    public String getRequestDesc() {
+        return requestDesc;
+    }
+
+    public void setRequestDesc(String requestDesc) {
+        this.requestDesc = requestDesc;
+    }
+
+    public String getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(String cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getApi() {
+        return api;
+    }
+
+    public void setApi(String api) {
+        this.api = api;
+    }
+
+    public String getPm25() {
+        return pm25;
+    }
+
+    public void setPm25(String pm25) {
+        this.pm25 = pm25;
+    }
+
+    public String getPm10() {
+        return pm10;
+    }
+
+    public void setPm10(String pm10) {
+        this.pm10 = pm10;
+    }
+
+    public String getCo() {
+        return co;
+    }
+
+    public void setCo(String co) {
+        this.co = co;
+    }
+
+    public String getSo2() {
+        return so2;
+    }
+
+    public void setSo2(String so2) {
+        this.so2 = so2;
+    }
+
+    public String getNo2() {
+        return no2;
+    }
+
+    public void setNo2(String no2) {
+        this.no2 = no2;
+    }
+
+    public String getO3() {
+        return o3;
+    }
+
+    public void setO3(String o3) {
+        this.o3 = o3;
+    }
+
+    public String getPrimary() {
+        return primary;
+    }
+
+    public void setPrimary(String primary) {
+        this.primary = primary;
+    }
 }
--- app/src/main/java/com/example/myapplication/data/http/entity/envicloud/EnvironmentCloudWeatherLive.java
+++ app/src/main/java/com/example/myapplication/data/http/entity/envicloud/EnvironmentCloudWeatherLive.java
@@ -1,4 +1,148 @@
 package com.example.myapplication.data.http.entity.envicloud;
 
+import com.alibaba.fastjson.annotation.JSONField;
+
+/**
+ * 天气实况
+ */
 public class EnvironmentCloudWeatherLive {
+
+    @JSONField(name = "rcode")
+    private int requestCode;        //结果码
+
+    @JSONField(name = "rdesc")
+    private String requestDesc;     //结果描述
+
+    @JSONField(name = "updatetime")
+    private String updateTime;      //更新时间
+
+    private String phenomena;       //天气现象
+
+    private String temperature;     //气温
+
+    @JSONField(name = "feelst")
+    private String feelsTemperature;    //体感温度
+
+    @JSONField(name = "airpressure")
+    private String airPressure;     //气压
+
+    private String humidity;        //相对湿度
+
+    private String rain;            //降雨量
+
+    @JSONField(name = "winddirect")
+    private String windDirect;      //风向
+
+    @JSONField(name = "windpower")
+    private String windPower;       //风力
+
+    @JSONField(name = "windspeed")
+    private String windSpeed;       //风速
+
+    @JSONField(name = "citycode")
+    private String cityId;          //城市ID
+
+    public int getRequestCode() {
+        return requestCode;
+    }
+
+    public void setRequestCode(int requestCode) {
+        this.requestCode = requestCode;
+    }
+
+    public String getRequestDesc() {
+        return requestDesc;
+    }
+
+    public void setRequestDesc(String requestDesc) {
+        this.requestDesc = requestDesc;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getPhenomena() {
+        return phenomena;
+    }
+
+    public void setPhenomena(String phenomena) {
+        this.phenomena = phenomena;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getFeelsTemperature() {
+        return feelsTemperature;
+    }
+
+    public void setFeelsTemperature(String feelsTemperature) {
+        this.feelsTemperature = feelsTemperature;
+    }
+
+    public String getAirPressure() {
+        return airPressure;
+    }
+
+    public void setAirPressure(String airPressure) {
+        this.airPressure = airPressure;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
+    }
+
+    public String getRain() {
+        return rain;
+    }
+
+    public void setRain(String rain) {
+        this.rain = rain;
+    }
+
+    public String getWindDirect() {
+        return windDirect;
+    }
+
+    public void setWindDirect(String windDirect) {
+        this.windDirect = windDirect;
+    }
+
+    public String getWindPower() {
+        return windPower;
+    }
+
+    public void setWindPower(String windPower) {
+        this.windPower = windPower;
+    }
+
+    public String getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(String windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public String getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(String cityId) {
+        this.cityId = cityId;
+    }
 }
