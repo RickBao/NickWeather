c94ac0a58aa57d2bcc5349b1f72ba7ca307bd280
Nick.mo <bjf@motern.cn>
2019-10-26 16:27:07 +0800

    添加小米天气对应bean文件具体内容

--- app/src/main/java/com/example/myapplication/data/http/entity/mi/MiToday.java
+++ app/src/main/java/com/example/myapplication/data/http/entity/mi/MiToday.java
@@ -1,4 +1,151 @@
 package com.example.myapplication.data.http.entity.mi;
 
 public class MiToday {
+
+    private int cityCode;
+    private String date;
+    private int humidityMax;
+    private int humidityMin;
+    private int precipitationMax;   //最大降水量
+    private int precipitationMin;   //最小降水量
+    private int tempMax;
+    private int tempMin;
+    private String weatherEnd;
+    private String weatherStart;
+    private String windDirectionEnd;
+    private String windDirectionStart;
+    private int windMax;
+    private int windMin;
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getHumidityMax() {
+        return humidityMax;
+    }
+
+    public void setHumidityMax(int humidityMax) {
+        this.humidityMax = humidityMax;
+    }
+
+    public int getHumidityMin() {
+        return humidityMin;
+    }
+
+    public void setHumidityMin(int humidityMin) {
+        this.humidityMin = humidityMin;
+    }
+
+    public int getPrecipitationMax() {
+        return precipitationMax;
+    }
+
+    public void setPrecipitationMax(int precipitationMax) {
+        this.precipitationMax = precipitationMax;
+    }
+
+    public int getPrecipitationMin() {
+        return precipitationMin;
+    }
+
+    public void setPrecipitationMin(int precipitationMin) {
+        this.precipitationMin = precipitationMin;
+    }
+
+    public int getTempMax() {
+        return tempMax;
+    }
+
+    public void setTempMax(int tempMax) {
+        this.tempMax = tempMax;
+    }
+
+    public int getTempMin() {
+        return tempMin;
+    }
+
+    public void setTempMin(int tempMin) {
+        this.tempMin = tempMin;
+    }
+
+    public String getWeatherEnd() {
+        return weatherEnd;
+    }
+
+    public void setWeatherEnd(String weatherEnd) {
+        this.weatherEnd = weatherEnd;
+    }
+
+    public String getWeatherStart() {
+        return weatherStart;
+    }
+
+    public void setWeatherStart(String weatherStart) {
+        this.weatherStart = weatherStart;
+    }
+
+    public String getWindDirectionEnd() {
+        return windDirectionEnd;
+    }
+
+    public void setWindDirectionEnd(String windDirectionEnd) {
+        this.windDirectionEnd = windDirectionEnd;
+    }
+
+    public String getWindDirectionStart() {
+        return windDirectionStart;
+    }
+
+    public void setWindDirectionStart(String windDirectionStart) {
+        this.windDirectionStart = windDirectionStart;
+    }
+
+    public int getWindMax() {
+        return windMax;
+    }
+
+    public void setWindMax(int windMax) {
+        this.windMax = windMax;
+    }
+
+    public int getWindMin() {
+        return windMin;
+    }
+
+    public void setWindMin(int windMin) {
+        this.windMin = windMin;
+    }
+
+    @Override
+    public String toString() {
+        return "MiToday{" +
+                "cityCode=" + cityCode +
+                ", date='" + date + '\'' +
+                ", humidityMax=" + humidityMax +
+                ", humidityMin=" + humidityMin +
+                ", precipitationMax=" + precipitationMax +
+                ", precipitationMin=" + precipitationMin +
+                ", tempMax=" + tempMax +
+                ", tempMin=" + tempMin +
+                ", weatherEnd='" + weatherEnd + '\'' +
+                ", weatherStart='" + weatherStart + '\'' +
+                ", windDirectionEnd='" + windDirectionEnd + '\'' +
+                ", windDirectionStart='" + windDirectionStart + '\'' +
+                ", windMax=" + windMax +
+                ", windMin=" + windMin +
+                '}';
+    }
 }
--- app/src/main/java/com/example/myapplication/data/http/entity/mi/MiWeather.java
+++ app/src/main/java/com/example/myapplication/data/http/entity/mi/MiWeather.java
@@ -1,4 +1,82 @@
 package com.example.myapplication.data.http.entity.mi;
 
+
+import com.alibaba.fastjson.annotation.JSONField;
+
+import java.util.List;
+
 public class MiWeather {
+
+    private MiForecast forecast;
+
+    @JSONField(name = "realtime")
+    private MiRealTime realTime;
+
+    private MiAQI aqi;
+
+    @JSONField(name = "index")
+    private List<MiIndex> indexList;
+
+    private MiToday today;
+    private MiToday yesterday;
+
+    public MiForecast getForecast() {
+        return forecast;
+    }
+
+    public void setForecast(MiForecast forecast) {
+        this.forecast = forecast;
+    }
+
+    public MiRealTime getRealTime() {
+        return realTime;
+    }
+
+    public void setRealTime(MiRealTime realTime) {
+        this.realTime = realTime;
+    }
+
+    public MiAQI getAqi() {
+        return aqi;
+    }
+
+    public void setAqi(MiAQI aqi) {
+        this.aqi = aqi;
+    }
+
+    public List<MiIndex> getIndexList() {
+        return indexList;
+    }
+
+    public void setIndexList(List<MiIndex> indexList) {
+        this.indexList = indexList;
+    }
+
+    public MiToday getToday() {
+        return today;
+    }
+
+    public void setToday(MiToday today) {
+        this.today = today;
+    }
+
+    public MiToday getYesterday() {
+        return yesterday;
+    }
+
+    public void setYesterday(MiToday yesterday) {
+        this.yesterday = yesterday;
+    }
+
+    @Override
+    public String toString() {
+        return "MiWeather{" +
+                "forecast=" + forecast +
+                ", realTime=" + realTime +
+                ", aqi=" + aqi +
+                ", indexList=" + indexList +
+                ", today=" + today +
+                ", yesterday=" + yesterday +
+                '}';
+    }
 }
